8a62a3304d3dcdf9f999f8ff89ede2bd71e48de6
Yakiv Mospan <yakiv.mospan@gmail.com>
2026-04-21 16:25:54 +0200

    Initial setup - added sample app and autocomplete library.

---
+++ app/src/main/java/com/yakivmospan/autocompletesample/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.yakivmospan.autocompletesample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yakivmospan.autocompletesample.lib.AutoCompleteComponent
+import com.yakivmospan.autocompletesample.ui.theme.AutocompleteSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AutocompleteSampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AutocompleteSampleApp(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AutocompleteSampleApp(innerPadding: PaddingValues) {
+    Box(modifier = Modifier.padding(innerPadding)) {
+        AutoCompleteComponent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AutocompleteSampleTheme {
+        AutocompleteSampleApp(PaddingValues())
+    }
+}
---
+++ autocomplete/src/main/java/com/yakivmospan/autocompletesample/lib/AutocompleteComponent.kt
@@ -0,0 +1,21 @@
+package com.yakivmospan.autocompletesample.lib
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AutoCompleteComponent(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Text(stringResource(R.string.autocomplete_title))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AutoCompleteComponentPreview() {
+    AutoCompleteComponent()
+}
